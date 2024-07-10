c4e99327f5e6f2ca965245f9441680d5e0c66a6f
insoo00 <insoosos1234@gmail.com>
2024-07-10 10:43:42 +0900

    :sparkles: [session2] variable

---
+++ java-intro/src/variable/Var.java
@@ -0,0 +1,32 @@
+package variable;
+
+public class Var {
+    public static void main(String[] args) {
+        byte b = 127;
+        System.out.println("byte = " + b);
+
+        short s = 32767;
+        System.out.println("short = " + s);
+
+        int i = 2147483647;
+        System.out.println("int = " + i);
+
+        long l = 9223372036854775807L;
+        System.out.println("long = " + l);
+
+        float f = 10.0f;
+        System.out.println("float = " + f);
+
+        double d = 10.0;
+        System.out.println("double = " + d);
+
+        boolean be = true;
+        System.out.println("boolean = " + be);
+
+        char c = 'A';
+        System.out.println("char = " + c);
+
+        String st = "Hello Java";
+        System.out.println("string = " + st);
+    }
+}
